3e63d1ac18c3354a4e9e44a998be62e38d0ebfa4
vivianle <g12b1@apex-prog-16938.local>
2016-01-21 12:55:32 -0600

    initial upload

---
+++ wed.php
@@ -0,0 +1,24 @@
+<?php
+
+function superhardmath($x, $y, $z) {
+  $b = $x + $y;
+
+  return $b * $z;
+}
+
+function areaofrect($base, $height) {
+  return $base * $height;
+}
+
+echo areaofrect(5, 5);
+
+?>
+
+//Ask user for shape
+//Ask user for shape input
+// //if square ask for side
+// //if triangle ask for base and height
+// //if circle ask for radius
+//output calculated area
+
+// TELL THE USER THAT YOU DON'T KNOW HOW TO CALUCULATE
